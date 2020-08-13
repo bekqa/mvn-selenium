13abf6d176c704bc4fbd217d4faf0de7b35cc57a
Ivan Petrov <info@mail.com>
2020-08-13 12:02:15 -0400

    Cleared Practice,java file

--- src/test/java/Practice.java
+++ src/test/java/Practice.java
@@ -1,32 +1,3 @@
 public class Practice {
 
-    /**
-     * Go here "https://the-internet.herokuapp.com/dropdown"
-     * Select option 1 from the dropdown
-     * Assert that option 1 is selected
-     * Assert taht option 2 is NOT selected
-     * */
-
-    /*
-     * Go to "https://the-internet.herokuapp.com/hovers"
-     * Hover over the first image
-     * Assert that on hover there is text that is displayed below "name: user1"
-     * */
-
-    /*
-     * https://the-internet.herokuapp.com/context_menu
-     * Right click
-     * close alert
-     * driver.switchTo().alert().accept();
-     * */
-
-    // go to "https://the-internet.herokuapp.com/key_presses"
-    // send right arrow key to the input box
-    // assert that you got this text back "You entered: RIGHT"
-
-    // go to https://ultimateqa.com/simple-html-elements-for-automation/
-    // find element with text "Clickable Icon"
-    // Assert href attribute =  https://ultimateqa.com/link-success/
-    // Get CSS value: "background-origin"
-    // Assert that it equals "padding-box"
 }
